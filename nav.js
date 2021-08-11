ea2b9aadf4605f42ff81e2f58ebe28e7bc2505a7
Leoval99 <leoval99@gmail.com>
2021-08-10 23:23:56 -0300

    initial commit

---
+++ nav.js
@@ -0,0 +1,6 @@
+btn.addEventListener("click", () => {
+    lista.classList.toggle("mostrar");
+});
+btn.addEventListener("click", () => {
+    btn.classList.toggle("btn-1");
+});
---
+++ whats.js
@@ -0,0 +1,7 @@
+whats.addEventListener("mouseover", () => {
+    contacto.classList.toggle("whats-contacto-2");
+});
+
+// whats.addEventListener("mouseout", () => {
+//     contacto.classList.toggle("whats-contacto");
+// });
